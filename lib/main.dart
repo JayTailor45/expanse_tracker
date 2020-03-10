97aeb9cf244a04d2dbd68f9d8e2d1c6f8d0b4073
JAY TAILOR <tailorj64@gmail.com>
2020-03-10 12:41:26 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(child: Text('CHART')),
+          Card(child: Text('List of Tx')),
+        ],
+      ),
+    );
+  }
+}
